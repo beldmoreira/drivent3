eb9a96d92a385cca3900a6561424f8d673ccb34a
Isabela <isabela_cmoreira@hotmail.com>
2023-02-06 14:06:58 -0300

    feat: created a few more factories to ensure better tests

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -10,12 +10,31 @@ export async function createHotel() {
   });
 }
 
+export async function createAnotherHotel() {
+  return prisma.hotel.create({
+    data: {
+      name: 'Hotel California',
+      image: 'https://lirp.cdn-website.com/3b09881a/dms3rep/multi/opt/2+escultura+sereia-1920w.jpg',
+    },
+  });
+}
+
 export async function createRoom(hotelId: number) {
   return prisma.room.create({
     data: {
       name: faker.company.catchPhraseAdjective(),
       capacity: faker.datatype.number({ min: 100, max: 1000 }),
       hotelId: hotelId,
+    },
+  });
+}
+
+export async function createAnotherRoom(hotelIId: number) {
+  return prisma.room.create({
+    data: {
+      name: 'Moroccan Style',
+      capacity: 70,
+      hotelId: hotelIId,
     },
   });
 }
--- tests/factories/tickets-factory.ts
+++ tests/factories/tickets-factory.ts
@@ -1,6 +1,6 @@
-import faker from "@faker-js/faker";
-import { prisma } from "@/config";
-import { TicketStatus } from "@prisma/client";
+import faker from '@faker-js/faker';
+import { prisma } from '@/config';
+import { TicketStatus } from '@prisma/client';
 
 export async function createTicketType() {
   return prisma.ticketType.create({
@@ -13,6 +13,38 @@ export async function createTicketType() {
   });
 }
 
+export async function createRemoteTicket() {
+  return prisma.ticketType.create({
+    data: {
+      name: faker.name.findName(),
+      price: faker.datatype.number(),
+      isRemote: true,
+      includesHotel: faker.datatype.boolean(),
+    },
+  });
+}
+
+export async function createTicketTypeWithHotel() {
+  return prisma.ticketType.create({
+    data: {
+      name: faker.name.findName(),
+      price: faker.datatype.number(),
+      isRemote: false,
+      includesHotel: true,
+    },
+  });
+}
+
+export async function createTicketTypeWithoutHotel() {
+  return prisma.ticketType.create({
+    data: {
+      name: faker.name.findName(),
+      price: faker.datatype.number(),
+      isRemote: false,
+      includesHotel: false,
+    },
+  });
+}
 export async function createTicket(enrollmentId: number, ticketTypeId: number, status: TicketStatus) {
   return prisma.ticket.create({
     data: {
